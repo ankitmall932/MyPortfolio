cf54ecdb148eed0e19830dfbaec7870f5a46f1f4
Ankit Mall <ankitmall932@gmail.com>
2026-05-28 13:51:24 +0530

    home logo fixe

--- portfolio/src/pages/Landing/Home.jsx
+++ portfolio/src/pages/Landing/Home.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 function Home () {
     return (
-        <div className='w-full  h-screen flex lg:flex-row flex-col-reverse  gap-5 '>
+        <div className='w-full  h-screen flex lg:flex-row flex-col-reverse  lg:gap-5 gap-3 '>
             <div className='lg:w-1/2 w-full h-full flex flex-col sm:gap-10 gap-5 justify-center items-center '>
                 <div className='bg-slate-900 text-cyan-400 border border-slate-800 sm:font-bold font-semibold sm:text-4xl text-2xl px-5 py-2 rounded mt-5 shadow-[0_0_25px_rgba(34,211,238,0.15)]'>Full Stack Developer</div>
                 <div className='bg-slate-900 text-cyan-400 border border-slate-800 sm:font-bold font-semibold sm:text-4xl text-2xl px-5 py-2 rounded'> Building Modern Web Applications</div>
@@ -13,8 +13,8 @@ function Home () {
                     <NavLink to='https://github.com/ankitmall932' className='border border-slate-700 text-slate-100 font-semibold hover:bg-slate-800 px-5 py-3 rounded-r hover:scale-105 transition-all duration-300'>GitHub</NavLink>
                 </div>
             </div>
-            <div className='lg:w-1/2 w-full  h-full flex justify-center items-center'>
-                <img src="/Ankit Mall.jpeg" alt="" className='object-cover sm:h-80 md:h-100 lg:h-120 xl:h-150 h-32 sm:w-80 md:w-100 lg:w-120 xl:w-150 w-32  rounded-full shadow-2xl' />
+            <div className='lg:w-1/2 w-full  lg:h-full h-70 flex justify-center items-center'>
+                <img src="/Ankit Mall.jpeg" alt="" className='object-cover sm:h-80 md:h-100 lg:h-120 xl:h-150 h-60 sm:w-80 md:w-100  lg:w-120 xl:w-150 w-60  rounded-full shadow-2xl lg:mt-0 mt-20' />
             </div>
         </div>
     );
